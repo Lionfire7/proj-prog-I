feada83b5854cdb58ecf8f5a0515c4aaee29eaa6
psousa13 <pedroesteves134@gmail.com>
2025-05-02 11:47:06 +0100

    menu com espaço de login de user funcional

--- Full-Program-Tester/main.c
+++ Full-Program-Tester/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "user.c"
 
 
 int menu (){
@@ -30,7 +31,7 @@ int menu (){
 
         case 2:
 
-            //utilizador
+            usermenu();
 
             break;
 
