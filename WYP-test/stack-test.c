cdee2704bb5d339cc7c36a3563a1fe850604aaf4
Lionfire7 <tiagosallionfire@gmail.com>
2025-05-26 11:51:54 +0100

    97% funcional

--- WYP-test/stack-test.c
+++ WYP-test/stack-test.c
@@ -8,16 +8,70 @@ typedef struct data {
 
 typedef struct llist {
     DATA data;
-    struct llist *next;
-} LIST;
+    struct llist* next;
+} llist;
 
 typedef struct stack {
-    LIST *head;
+    llist* head;
 }STACK;
+
+int isEmpty(STACK* stack) {
+  
+    return stack->head == NULL;
+}
+
 STACK *criar_stack(DATA ndata){
-    STACK *stack = (STACK *)malloc(sizof(STACK));
+    STACK *stack = (STACK *)malloc(sizeof(STACK));
     if (stack == NULL) {
         fprintf(stderr, "Memory allocation failed\n");
         exit(EXIT_FAILURE);
     }
+    llist *new_node = (llist *)malloc(sizeof(llist));
+    if (new_node == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(EXIT_FAILURE);
+    }
+    new_node->data = ndata;
+    new_node->next = NULL;
+    stack->head = new_node;
+    return stack;
+}
+
+void initializeStack(STACK* stack) { stack->head = NULL; }
+
+void push(STACK* stack, DATA ndata){
+  
+    llist* new_node = (llist*)malloc(sizeof(llist));
+    
+    new_node->data = ndata;
+    new_node->next = stack->head;
+    stack->head = new_node;
+
+}
+
+DATA peek(STACK* stack) {
+  
+    if (!isEmpty(stack))
+        return stack->head->data;
+    else {
+        printf("\nStack is empty");
+        return (DATA){-1, -1};
+    }
+}
+
+int main() {
+
+    STACK stack;
+    initializeStack(&stack);
+
+    push(&stack, (DATA){11, 11});
+    push(&stack, (DATA){22, 22});
+    push(&stack, (DATA){33, 33});
+    push(&stack, (DATA){44, 44});
+
+    printf("Top element is %d\n,%d\n ", peek(&stack));
+
+    printf("Top element is %d\n,%d\n ", peek(&stack));
+
+    return 0;
 }
